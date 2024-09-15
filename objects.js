f73fbfa4bbfce88ea43ab09cf4811b7fa1b68357
Ycar <37698893+Ycarx@users.noreply.github.com>
2024-09-15 22:14:31 +0200

    init

---
+++ objects.js
@@ -0,0 +1,7 @@
+const objetsDetails = {
+    "pilotNote": {
+        name: "Note de Pilote",
+        description: "Cette note contient des informations sur le passé de Magellus."
+    }
+    // Ajoute tous les autres objets ici
+};
